23819954ca53b0fb3f156d504b5956da13727368
Adarsh Narayan <adarshnarayangumla@gmail.com>
2025-01-20 19:44:51 +0530

    docs: add comment to clarify JournalEntry model structure

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+//like model in node(mongoose)
 @Document(collection = "journal_entries")
 @Data
 @NoArgsConstructor
